467d4721c967713631c669802027f1f0a6ae2564
NekitJavaDev <blake_nik@icloud.com>
2019-04-06 02:43:13 +0300

    Added Orika Mapper Facade for convert Entity to View

---
+++ src/main/java/ru/homecompany/bank/service/mapper/CustomMapperFactory.java
@@ -0,0 +1,26 @@
+package ru.homecompany.bank.service.mapper;
+
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomMapperFactory implements FactoryBean<MapperFactory> {
+    @Override
+    public MapperFactory getObject() {
+        return new DefaultMapperFactory.Builder()
+                .constructorResolverStrategy(null)
+                .build();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return MapperFactory.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+}
