d1e2555a217cb9caed0cbd58ee5c66c060eb856d
NekitJavaDev <blake_nik@icloud.com>
2019-04-05 03:10:22 +0300

    Added EmployeeDao interface and basic implementation of it

--- src/main/java/ru/homecompany/bank/dao/employee/EmployeeDao.java
+++ src/main/java/ru/homecompany/bank/dao/employee/EmployeeDao.java
@@ -1,59 +1,59 @@
-//package ru.homecompany.bank.dao.employee;
-//
-//
-//import ru.homecompany.bank.model.Employee;
-//
-//import java.util.List;
-//
-///**
-// * DAO for working with employees
-// */
-//public interface EmployeeDao {
+package ru.homecompany.bank.dao.employee;
+
+
+import ru.homecompany.bank.model.Employee;
+
+import java.util.List;
+
+/**
+ * DAO for working with employees
+ */
+public interface EmployeeDao {
+
+    /**
+     * Get all employees
+     *
+     * @return {@Employees}
+     */
+    List<Employee> findAll();
+
+    /**
+     * Get employee by ID
+     *
+     * @param id
+     * @return @Employee
+     */
+    Employee findById(Integer id);
 //
 //    /**
-//     * Get all employees
+//     * Get employees by his first name
 //     *
-//     * @return {@Employees}
+//     * @param firstName
+//     * @return {@Employee}
 //     */
-//    List<Employee> findAll();
+//    List<Employee> findByFirstName(String firstName);
 //
-////    /**
-////     * Get employee by ID
-////     *
-////     * @param id
-////     * @return @Employee
-////     */
-////    Employee findById(Integer id);
-////
-////    /**
-////     * Get employees by his first name
-////     *
-////     * @param firstName
-////     * @return {@Employee}
-////     */
-////    List<Employee> findByFirstName(String firstName);
-////
-////    /**
-////     * Get employees by his position at work
-////     *
-////     * @param position
-////     * @return {@Employee}
-////     */
-////    List<Employee> findByPosition(String position);
-////
-////    /**
-////     * Get an employee by his identity document ID
-////     *
-////     * @param documentId
-////     * @return {@Employee}
-////     */
-////    Employee findByDocumentId(Integer documentId);
-////
-////    /**
-////     * Get employees by info where they born
-////     *
-////     * @param countryId
-////     * @return {@Employee}
-////     */
-////    List<Employee> findByCountryId(Integer countryId);
-//}
+//    /**
+//     * Get employees by his position at work
+//     *
+//     * @param position
+//     * @return {@Employee}
+//     */
+//    List<Employee> findByPosition(String position);
+//
+//    /**
+//     * Get an employee by his identity document ID
+//     *
+//     * @param documentId
+//     * @return {@Employee}
+//     */
+//    Employee findByDocumentId(Integer documentId);
+//
+//    /**
+//     * Get employees by info where they born
+//     *
+//     * @param countryId
+//     * @return {@Employee}
+//     */
+//    List<Employee> findByCountryId(Integer countryId);
+}
--- src/main/java/ru/homecompany/bank/dao/employee/EmployeeDaoImpl.java
+++ src/main/java/ru/homecompany/bank/dao/employee/EmployeeDaoImpl.java
@@ -1,134 +1,147 @@
-//package ru.homecompany.bank.dao.employee;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Repository;
-//import ru.homecompany.bank.model.Employee;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.TypedQuery;
-//import javax.persistence.criteria.CriteriaBuilder;
-//import javax.persistence.criteria.CriteriaQuery;
-//import javax.persistence.criteria.Root;
-//import java.util.List;
-//
-///**
-// * Implementation of repository EmployeeDao
-// */
-//@Repository
-//public class EmployeeDaoImpl implements EmployeeDao {
-//
-//    private EntityManager em;
+package ru.homecompany.bank.dao.employee;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ru.homecompany.bank.model.Employee;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+/**
+ * Implementation of repository EmployeeDao
+ */
+@Repository
+public class EmployeeDaoImpl implements EmployeeDao {
+
+    private EntityManager em;
+
+    /**
+     * Dependency injection of JPA EntityManager implementation through public constructor
+     *
+     * @param em
+     */
+    @Autowired
+    public EmployeeDaoImpl(EntityManager em) {
+        this.em = em;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Employee> findAll() {
+        CriteriaQuery<Employee> criteriaQuery = buildCriteriaAll();
+        TypedQuery<Employee> query = em.createQuery(criteriaQuery);
+        return query.getResultList();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Employee findById(Integer id) {
+        CriteriaQuery<Employee> criteriaQuery = buildCriteriaId(id);
+        TypedQuery<Employee> query = em.createQuery(criteriaQuery);
+        return query.getSingleResult();
+    }
+
+    private CriteriaQuery<Employee> buildCriteriaId(Integer id) {
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
+        Root<Employee> employeeRoot = criteriaQuery.from(Employee.class);
+        criteriaQuery.where(criteriaBuilder.equal(employeeRoot.get("id"), id));
+        criteriaQuery.select(employeeRoot);
+        return criteriaQuery;
+    }
+
+    private CriteriaQuery<Employee> buildCriteriaAll() {
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
+        Root<Employee> employeeRoot = criteriaQuery.from(Employee.class);
+        criteriaQuery.select(employeeRoot);
+        return criteriaQuery;
+    }
+
+
 //
 //    /**
-//     * Dependency injection of JPA EntityManager implementation through public constructor
-//     *
-//     * @param em
+//     * {@inheritDoc}
 //     */
-//    @Autowired
-//    public EmployeeDaoImpl(EntityManager em) {
-//        this.em = em;
+//    @Override
+//    public List<Employee> findByFirstName(String firstName) {
+//        CriteriaQuery<Employee> criteriaQuery = buildCriteriaName(firstName);
+//        TypedQuery<Employee> query = em.createQuery(criteriaQuery);
+//        return query.getResultList();
 //    }
 //
 //    /**
 //     * {@inheritDoc}
 //     */
 //    @Override
-//    public List<Employee> findAll() {
-//        CriteriaQuery<Employee> criteriaQuery = buildCriteriaAll();
+//    public List<Employee> findByPosition(String position) {
+//        CriteriaQuery<Employee> criteriaQuery = buildCriteriaPosition(position);
 //        TypedQuery<Employee> query = em.createQuery(criteriaQuery);
 //        return query.getResultList();
 //    }
 //
-//    private CriteriaQuery<Employee> buildCriteriaAll() {
+//    /**
+//     * {@inheritDoc}
+//     */
+//    @Override
+//    public Employee findByDocumentId(Integer documentId) {
+//        CriteriaQuery<Employee> criteriaQuery = buildCriteriaDocument(documentId);
+//        TypedQuery<Employee> query = em.createQuery(criteriaQuery);
+//        return query.getSingleResult();
+//    }
+//
+//    /**
+//     * {@inheritDoc}
+//     */
+//    @Override
+//    public List<Employee> findByCountryId(Integer countryId) {
+//        CriteriaQuery<Employee> criteriaQuery = buildCriteriaCountry(countryId);
+//        TypedQuery<Employee> query = em.createQuery(criteriaQuery);
+//        return query.getResultList();
+//    }
+//
+//    private CriteriaQuery<Employee> buildCriteriaDocument(Integer documentId) {
 //        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
 //        CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
-//        Root<Employee> employeeRoot = criteriaQuery.from(Employee.class);
-//        criteriaQuery.select(employeeRoot);
+//        Root<Employee> employee = criteriaQuery.from(Employee.class);
+//        criteriaQuery.where(criteriaBuilder.equal(employee.get("documentId"), documentId));
+//        criteriaQuery.select(employee);
+//        return criteriaQuery;
+//    }
+
+
+//    private CriteriaQuery<Employee> buildCriteriaName(String firstName) {
+//        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+//        CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
+//        Root<Employee> employee = criteriaQuery.from(Employee.class);
+//        criteriaQuery.where(criteriaBuilder.equal(employee.get("firstName"), firstName));
+//        criteriaQuery.select(employee);
 //        return criteriaQuery;
 //    }
 //
-////    /**
-////     * {@inheritDoc}
-////     */
-////    @Override
-////    public Employee findById(Integer id) {
-////        return null;
-////    }
-////
-////    /**
-////     * {@inheritDoc}
-////     */
-////    @Override
-////    public List<Employee> findByFirstName(String firstName) {
-////        CriteriaQuery<Employee> criteriaQuery = buildCriteriaName(firstName);
-////        TypedQuery<Employee> query = em.createQuery(criteriaQuery);
-////        return query.getResultList();
-////    }
-////
-////    /**
-////     * {@inheritDoc}
-////     */
-////    @Override
-////    public List<Employee> findByPosition(String position) {
-////        CriteriaQuery<Employee> criteriaQuery = buildCriteriaPosition(position);
-////        TypedQuery<Employee> query = em.createQuery(criteriaQuery);
-////        return query.getResultList();
-////    }
-////
-////    /**
-////     * {@inheritDoc}
-////     */
-////    @Override
-////    public Employee findByDocumentId(Integer documentId) {
-////        CriteriaQuery<Employee> criteriaQuery = buildCriteriaDocument(documentId);
-////        TypedQuery<Employee> query = em.createQuery(criteriaQuery);
-////        return query.getSingleResult();
-////    }
-////
-////    /**
-////     * {@inheritDoc}
-////     */
-////    @Override
-////    public List<Employee> findByCountryId(Integer countryId) {
-////        CriteriaQuery<Employee> criteriaQuery = buildCriteriaCountry(countryId);
-////        TypedQuery<Employee> query = em.createQuery(criteriaQuery);
-////        return query.getResultList();
-////    }
-////
-////    private CriteriaQuery<Employee> buildCriteriaDocument(Integer documentId) {
-////        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-////        CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
-////        Root<Employee> employee = criteriaQuery.from(Employee.class);
-////        criteriaQuery.where(criteriaBuilder.equal(employee.get("documentId"), documentId));
-////        criteriaQuery.select(employee);
-////        return criteriaQuery;
-////    }
+//    private CriteriaQuery<Employee> buildCriteriaPosition(String position) {
+//        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+//        CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
+//        Root<Employee> employee = criteriaQuery.from(Employee.class);
+//        criteriaQuery.where(criteriaBuilder.equal(employee.get("position"), position));
+//        criteriaQuery.select(employee);
+//        return criteriaQuery;
+//    }
 //
-//
-////    private CriteriaQuery<Employee> buildCriteriaName(String firstName) {
-////        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-////        CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
-////        Root<Employee> employee = criteriaQuery.from(Employee.class);
-////        criteriaQuery.where(criteriaBuilder.equal(employee.get("firstName"), firstName));
-////        criteriaQuery.select(employee);
-////        return criteriaQuery;
-////    }
-////
-////    private CriteriaQuery<Employee> buildCriteriaPosition(String position) {
-////        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-////        CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
-////        Root<Employee> employee = criteriaQuery.from(Employee.class);
-////        criteriaQuery.where(criteriaBuilder.equal(employee.get("position"), position));
-////        criteriaQuery.select(employee);
-////        return criteriaQuery;
-////    }
-////
-////    private CriteriaQuery<Employee> buildCriteriaCountry(Integer countryId) {
-////        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-////        CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
-////        Root<Employee> employee = criteriaQuery.from(Employee.class);
-////        criteriaQuery.where(criteriaBuilder.equal(employee.get("info"), countryId));
-////        criteriaQuery.select(employee);
-////        return criteriaQuery;
-////    }
-//}
+//    private CriteriaQuery<Employee> buildCriteriaCountry(Integer countryId) {
+//        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+//        CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
+//        Root<Employee> employee = criteriaQuery.from(Employee.class);
+//        criteriaQuery.where(criteriaBuilder.equal(employee.get("info"), countryId));
+//        criteriaQuery.select(employee);
+//        return criteriaQuery;
+//    }
+}
